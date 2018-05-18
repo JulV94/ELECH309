6de4c6acf8ce668c875efe8d8652a5b1d6cb30f2
JulV94 <julvdev@gmail.com>
2018-05-18 16:31:12 +0200

    add uart and adapt regulation

--- codes/regulation/main.c
+++ codes/regulation/main.c
@@ -8,11 +8,30 @@
     #define M_PI 3.141592653f
 #endif
 #define ACCEL 0.3
-#define ANG_ACCEL ACCEL
 #define MAX_SPEED 0.4
 #define KP_POS 8.0/20
-#define KP_ROT 10.9/(20*5)
-#define TOL -0.001
+#define KP_ROT 2.18/20
+#define TOL 0.002
+#define ROT_TOL 2
+#define DEAD_ZONE 0.005
+
+#define THROTTLE 0.5
+#define ANG_THROTTLE 0.5
+
+typedef enum cmd {
+    STRAIGHT = 0x00,
+    RIGHT = 0x01,
+    LEFT = 0x02
+} cmd;
+
+float floatAbs(float value)
+{
+    if (value < 0)
+    {
+        return -value;
+    }
+    return value;
+}
 
 void encodersInit() {
     RPINR14bits.QEA1R = 19;//left encoder
@@ -35,11 +54,13 @@ void motorsInit() {
     //Output compare 1
     OC1CONbits.OCTSEL = 0; //selectionne le timer 2
     OC1R = 0*PR2; // nb d'instructions � l'�tat haut (Ton) = (Ton/T)*PR2
+    OC1RS = 0;
     OC1CONbits.OCM = 0b110; //fault pin disabled
 
     //Output compare 2
     OC2CONbits.OCTSEL = 0; //selectionne le timer 2
     OC2R = 0*PR2; // nb d'instructions � l'etat haut (Ton) = (Ton/T)*PR2
+    OC2RS = 0;
     OC2CONbits.OCM = 0b110; //fault pin disabled
 }
 
@@ -66,38 +87,38 @@ void UART1Init()
     U1STAbits.UTXEN = 1;  // activate transmission
 }
 //regulateur de translation
-float posReg(float reference) {
-    return KP_POS*(reference - (((int)POS1CNT + (int)POS2CNT)*M_PI*D/720));
+float posReg(float reference, int offset1, int offset2) {
+    return KP_POS*(reference - (((int)POS1CNT + (int)POS2CNT - offset1 - offset2)*M_PI*D/720));
 }
 //regulateur de rotation
-float rotReg(float reference) {
-    //float tmp;
-
-    //tmp = ((int)POS1CNT - (int)POS2CNT);
-    //if ((tmp < -50) || (tmp > 50))
-        //tmp += 1;
-    return KP_ROT*(reference - (((int)POS1CNT - (int)POS2CNT)*(D*M_PI)/(180)));
+float rotReg(float reference, int offset1, int offset2) {
+    return KP_ROT*(reference - (((int)POS1CNT - (int)POS2CNT - offset1 + offset2)*M_PI*D/360));
 }
 
 float genReference(float pos, float speed, float dt) {
-  if (speed*speed/ACCEL < pos)
+    int sign = 1;
+    if (pos < 0)
+    {
+        sign = -1;
+    }
+  if (sign*speed*speed/ACCEL < pos)
   {
     // Trapeze
     if (dt < speed/ACCEL)
     {
       // Acceleration part
-      return ACCEL*dt*dt/2;
+      return sign*ACCEL*dt*dt/2;
     }
-    else if (dt < pos/speed)
+    else if (dt < sign*pos/speed)
     {
       // Flat speed part
-      return speed*dt - speed*speed/(2*ACCEL);
+      return sign*speed*dt - sign*speed*speed/(2*ACCEL);
     }
-    else if(dt < pos/speed + speed/ACCEL)//ajout de condition sur t3
+    else if(dt < sign*pos/speed + speed/ACCEL)//ajout de condition sur t3
     {
       // Deceleration part
-      float newDt = dt - pos/speed;
-      return pos - speed*speed/(2*ACCEL) + speed*newDt - ACCEL*newDt*newDt/2;
+      float newDt = dt - sign*pos/speed;
+      return pos - sign*speed*speed/(2*ACCEL) + sign*speed*newDt - sign*ACCEL*newDt*newDt/2;
     }
     else{
         return pos;// at zero speed, the position is constant.
@@ -106,82 +127,60 @@ float genReference(float pos, float speed, float dt) {
   else
   {
     // Triangle
-    if (dt*dt < pos/ACCEL)
+    if (dt*dt < sign*pos/ACCEL)
     {
       // Acceleration part
-      return ACCEL*dt*dt/2;
+      return sign*ACCEL*dt*dt/2;
     }
-    else
+    else if (2*dt*dt < sign*pos/ACCEL)
     {
       // Deceleration part
-      float newDt = dt - sqrt(pos/ACCEL);
-      return pos/2 + ACCEL*sqrt(pos/ACCEL)*newDt - ACCEL*newDt*newDt/2;
-    }
-  }
-}
-
-float genRotReference(float pos, float speed, float dt) {
-  if (speed*speed/ANG_ACCEL < pos)
-  {
-    // Trapeze
-    if (dt < speed/ANG_ACCEL)
-    {
-      // Acceleration part
-      return ANG_ACCEL*dt*dt/2;
-    }
-    else if (dt < pos/speed)
-    {
-      // Flat speed part
-      return speed*dt - speed*speed/(2*ANG_ACCEL);
-    }
-    else if(dt < pos/speed + speed/ANG_ACCEL)//ajout de condition sur t3
-    {
-      // Deceleration part
-      float newDt = dt - pos/speed;
-      return pos - speed*speed/(2*ANG_ACCEL) + speed*newDt - ANG_ACCEL*newDt*newDt/2;
+      float newDt = dt - sqrt(sign*pos/ACCEL);
+      return pos/2 + sign*ACCEL*sqrt(sign*pos/ACCEL)*newDt - sign*ACCEL*newDt*newDt/2;
     }
     else{
         return pos;// at zero speed, the position is constant.
     }
   }
-  else
-  {
-    // Triangle
-    if (dt*dt < pos/ANG_ACCEL)
-    {
-      // Acceleration part
-      return ANG_ACCEL*dt*dt/2;
-    }
-    else
-    {
-      // Deceleration part
-      float newDt = dt - sqrt(pos/ANG_ACCEL);
-      return pos/2 + ANG_ACCEL*sqrt(pos/ANG_ACCEL)*newDt - ANG_ACCEL*newDt*newDt/2;
-    }
-  }
 }
-
-
 
 //A l'aide du PWM, on envoie des commandes au moteur en choissisant T=10ms. Les moteurs sont alimentee soit en tension +ve ou -ve
 void sendMotorCmd(float cmd1, float cmd2) {
-  if (cmd1 < 0.1)
-  {
-      cmd1 = 0.1;
-  }
-  else if (cmd1 > 0.2)
-  {
-      cmd1 = 0.2;
-  }
-
-  if (cmd2 > 0.2)
-  {
-      cmd2 = 0.2;
-  }
-  else if (cmd2 < 0.1)
-  {
-      cmd2 = 0.1;
-  }
+    if (cmd1 < 0.15)
+    {
+        cmd1 -= DEAD_ZONE;
+    }
+    else if (cmd1 > 0.15)
+    {
+        cmd1 += DEAD_ZONE;
+    }
+    
+    if (cmd1 < 0.1)
+    {
+        cmd1 = 0.1;
+    }
+    else if (cmd1 > 0.2)
+    {
+        cmd1 = 0.2;
+    }
+    
+    if (cmd2 < 0.15)
+    {
+        cmd2 -= DEAD_ZONE;
+    }
+    else if (cmd2 > 0.15)
+    {
+        cmd2 += DEAD_ZONE;
+    }
+    
+    if (cmd2 < 0.1)
+    {
+        cmd2 = 0.1;
+    }
+    else if (cmd2 > 0.2)
+    {
+        cmd2 = 0.2;
+    }
 //envoie de la tension aux moteurs si conditions ci dessus sont remplies
   OC1RS = cmd1*PR2;
   OC2RS = cmd2*PR2;
@@ -189,6 +188,7 @@ void sendMotorCmd(float cmd1, float cmd2) {
 //fonction qui permet de r�gler la translation
 void translate(float d, float speed) {
     float cmdPos, cmdRot;
+    int offsetP1 = (int)POS1CNT, offsetP2 = (int)POS2CNT;
     float cnt = 0; //compteur temps
     T1CONbits.TON = 1; //activation timer
     do {
@@ -196,11 +196,11 @@ void translate(float d, float speed) {
         {
             IFS0bits.T1IF = 0;
             cnt += 0.01; //+10ms
-            cmdPos = posReg(genReference(d, speed*MAX_SPEED,cnt));
-            cmdRot = rotReg(0);
+            cmdPos = posReg(genReference(d, speed*MAX_SPEED,cnt), offsetP1, offsetP2);
+            cmdRot = rotReg(0, offsetP1, offsetP2);
             sendMotorCmd(0.15 + cmdPos + cmdRot, 0.15 - (cmdPos - cmdRot));
          }
-    }while((((int)POS1CNT + (int)POS2CNT)*M_PI*D/720) - d < TOL);
+    }while(floatAbs((((int)POS1CNT + (int)POS2CNT - offsetP1 - offsetP2)*M_PI*D/720.0) - d) > TOL);
     T1CONbits.TON = 0;
     OC1RS = 0;
     OC2RS = 0;
@@ -208,6 +208,7 @@ void translate(float d, float speed) {
 //fonction qui permet de r�gler la rotation du robot
 void rotate(float theta, float angSpeed) {
     float cmdPos, cmdRot;
+    int offsetP1 = (int)POS1CNT, offsetP2 = (int)POS2CNT;
     float cnt = 0; //compteur temps
     T1CONbits.TON = 1; //activation timer
     do {
@@ -215,11 +216,11 @@ void rotate(float theta, float angSpeed) {
         {
             IFS0bits.T1IF = 0;
             cnt += 0.01; //+10ms
-            cmdPos = posReg(0);
-            cmdRot = rotReg(genReference(theta*E/2, angSpeed*MAX_SPEED,cnt));
+            cmdPos = posReg(0, offsetP1, offsetP2);
+            cmdRot = rotReg(genReference(theta*E/2, angSpeed*MAX_SPEED,cnt), offsetP1, offsetP2);
             sendMotorCmd(0.15 + cmdPos + cmdRot, 0.15 - (cmdPos - cmdRot));
         }
-    }while(((int)POS1CNT - (int)POS2CNT)*(D*M_PI/(360*E)) - theta < -0.08);
+    }while(floatAbs(((int)POS1CNT - (int)POS2CNT - offsetP1 + offsetP2)*(D/(2*E)) - theta) > ROT_TOL);
     T1CONbits.TON = 0;
     OC1RS = 0;
     OC2RS = 0;
@@ -231,21 +232,39 @@ int main(void) {
     encodersInit();
     timersInit();
     motorsInit();
+    
+    // Init UART1
     UART1Init();
+    RPINR18bits.U1RXR = 6; // Configure RP6 as UART1 Rx
+    RPOR3bits.RP7R = 3;    // Configure RP7 as UART1 Tx
 
     // TODO remove that
-    //translate(1, 0.5);
-    rotate(2*M_PI, 0.5);
+    //translate(0.1, 0.5);
+    //rotate(2*M_PI, 0.5);
 
-    char rxReg;
+    cmd rxCmd;
+    char rxParam;
 
     while(1)
     {
         if (U1STAbits.URXDA)
         {
-            rxReg = U1RXREG;
-            // TODO process data received
-            // TODO perform asked task
+            rxCmd = (cmd)(U1RXREG);
+            while(!U1STAbits.URXDA);
+            rxParam = U1RXREG;
+            switch(rxCmd) {
+                case STRAIGHT:
+                    translate((float)(rxParam)/100.0, THROTTLE);  // To get cm
+                    break;
+                case RIGHT:
+                    rotate((float)(rxParam), ANG_THROTTLE);
+                    break;
+                case LEFT:
+                    rotate(-(float)(rxParam), ANG_THROTTLE);
+                    break;
+                default:
+                    break;
+            }
         }
 	  }
 }
